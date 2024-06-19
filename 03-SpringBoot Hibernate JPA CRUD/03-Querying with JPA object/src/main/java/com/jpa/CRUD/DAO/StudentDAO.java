db525cddf9203995f7984879a4cc1b9ae034131c
Jagadeesh T <jagadeesh.t@technoforte.co.in>
2024-06-19 17:15:03 +0530

    JPA

---
+++ 03-SpringBoot Hibernate JPA CRUD/03-Querying with JPA object/src/main/java/com/jpa/CRUD/DAO/StudentDAO.java
@@ -0,0 +1,14 @@
+package com.jpa.CRUD.DAO;
+
+import java.util.List;
+
+import com.jpa.CRUD.Entity.Student;
+
+public interface StudentDAO {
+	
+	void save(Student theStudent);
+	Student finfById(Integer id);
+	List<Student> findAll();
+	List<Student> findByLastName(String theLastName);
+	
+}
